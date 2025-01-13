c355984dde044a86dcc99979b9478444fa7f628d
Noorul Ameen <noorulameen9220@gmail.com>
2025-01-13 18:01:37 +0530

    modified

--- components/about-me.js
+++ components/about-me.js
@@ -16,7 +16,7 @@ export function AboutMe() {
         <Card>
           <CardContent className="p-6">
   <p className="text-lg mb-4" >
-   <b> Hi, I’m Noorul Ameen! </b><br></br>
+   <b> Hi, I&apos;m Noorul Ameen! </b><br></br>
      I am a Bachelor's student in Computer Science with a strong passion for technology and innovation. My skills include programming, working with AI, and solving problems using new technologies. I’ve also dabbled in graphic design, making cool flyers and content at a local restaurant using Canva. I have gained valuable experience as an AI Trainer at Outlier and as a Software Engineering Fellow at Headstarter AI, where I developed my technical and leadership skills.
             </p>
             
