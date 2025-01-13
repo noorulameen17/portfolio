eb1c445e5942fe75d4f15a39db65acb5de1704a7
Noorul Ameen <noorulameen9220@gmail.com>
2025-01-13 18:10:10 +0530

    modified

--- components/about-me.js
+++ components/about-me.js
@@ -15,19 +15,27 @@ export function AboutMe() {
         <h2 className="text-3xl font-bold mb-8 text-center">About Me</h2>
         <Card>
           <CardContent className="p-6">
-  <p className="text-lg mb-4" >
-   <b> Hi, I&apos;m Noorul Ameen! </b><br></br>
-     I am a Bachelor's student in Computer Science with a strong passion for technology and innovation. My skills include programming, working with AI, and solving problems using new technologies. I’ve also dabbled in graphic design, making cool flyers and content at a local restaurant using Canva. I have gained valuable experience as an AI Trainer at Outlier and as a Software Engineering Fellow at Headstarter AI, where I developed my technical and leadership skills.
+            <p className="text-lg mb-4">
+              <b> Hi&#44; I&apos;m Noorul Ameen&#33; </b>
+              <br></br>I am a Bachelor&apos;s student in Computer Science with a
+              strong passion for technology and innovation. My skills include
+              programming&#44; working with AI&#44; and solving problems using
+              new technologies. I&apos;ve also dabbled in graphic design&#44;
+              making cool flyers and content at a local restaurant using Canva.
+              I have gained valuable experience as an AI Trainer at Outlier and
+              as a Software Engineering Fellow at Headstarter AI&#44; where I
+              developed my technical and leadership skills.
             </p>
-            
-  <p className="text-lg mb-4">
-    Lowkey obsessed with AI tools - not just for work, but for pretty much everything <b>(yep, everything)</b>. Always learning, always leveling up!
-    </p>
-              
+
+            <p className="text-lg mb-4">
+              Lowkey obsessed with AI tools &#8208; not just for work&#44; but
+              for pretty much everything <b>&#40;yep&#44; everything&#41;</b>.
+              Always learning&#44; always leveling up!
+            </p>
           </CardContent>
         </Card>
       </motion.div>
     </section>
-  )
+  );
 }
 
