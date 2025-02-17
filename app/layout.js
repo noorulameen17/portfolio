b010601ea3f1ba860e79908c5bb36d2bacb6afcd
Noorul Ameen <noorulameen9220@gmail.com>
2025-02-17 23:29:58 +0530

    add vercel analytics

--- app/layout.js
+++ app/layout.js
@@ -3,13 +3,16 @@ import { Header } from '../components/header';
 import { Footer } from '../components/footer';
 import './globals.css';
 import { CustomThemeProvider } from "../components/theme-provider";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Noorul Ameen Portfolio',
-  description: 'Portfolio of Noorul Ameen, a graphic designer proficient in Canva and an aspiring software engineer.',
+  title: "Noorul Ameen | Software Engineer & Frontend Developer Portfolio",
+  description:
+    "Official portfolio of Noorul Ameen, a Software Engineer & Frontend Developer skilled in React, Next.js, JavaScript, Supabase, Material-UI, Magic UI, Motion, Clerk, and more. Explore projects, graphic designs, and expertise in building modern web applications.",
 };
+
 
 export default function RootLayout({ children }) {
   return (
@@ -22,7 +25,7 @@ export default function RootLayout({ children }) {
           disableTransitionOnChange
         >
           <Header />
-          <main className="min-h-screen">{children}</main>
+          <main className="min-h-screen">{children}<Analytics/></main>
           <Footer />
         </CustomThemeProvider>
       </body>
